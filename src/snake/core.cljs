b25224321039186abfe8441724a2e80a5ab0a2e4
Duncan Brown <duncanjbrown@gmail.com>
2018-10-02 21:12:30 +0100

    Pass snake to board as a cursor
    Make sure it's always a vector of vectors, because lists don't work (we drop the
    element we add, beause it's added to the front)

--- src/snake/core.cljs
+++ src/snake/core.cljs
@@ -1,7 +1,9 @@
 (ns ^:figwheel-always snake.core
   (:require [reagent.core :as reagent]
-            [snake.board :as board]))
+            [snake.board :as board]
+            [snake.snake :as snk]))
 
+(defonce state (reagent/atom {:snake (snk/new-snake)}))
 
 (when-let [element (js/document.getElementById "app")]
-  (reagent/render-component [board/board (board/build-board)] element))
+  (reagent/render-component [board/game (reagent/cursor state [:snake])] element))
--- src/snake/snake.cljs
+++ src/snake/snake.cljs
@@ -4,10 +4,12 @@
   []
   (let [length 4
         start-pos [5 4]]
-    (take length (iterate #(vec (map + [0 1] %)) start-pos))))
+    (vec (take length (iterate #(vec (map + [0 1] %)) start-pos)))))
 
 (defn move-snake
   [snake movement-vector board-size]
-  (let [new-head-position (map #(mod % 10)
-                               (vec (map + (last snake) movement-vector)))]
-    (drop 1 (conj snake new-head-position))))
+  (let [new-head-position (vec (map #(mod % 10)
+                                    (map + (last snake) movement-vector)))]
+    (vec (drop 1 (conj snake new-head-position)))))
+
+;; (move-snake [[0 1] [0 2]] [0 1] 10)
