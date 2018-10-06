ab8e2a349faba7b6407e81d299a2fa036d90d419
Duncan Brown <duncanjbrown@gmail.com>
2018-10-06 23:35:40 +0100

    Write the snake into the board
    Add snake.board.populate, which takes a board and a set of coords (ie a snake)
    and returns the board vector with the snake co-ords marked.
    
    Add snake.board.board-cell, which controls how those marked elements are
    displayed.

--- src/snake/board.cljs
+++ src/snake/board.cljs
@@ -3,14 +3,27 @@
 (def width 10)
 (def height 10)
 
-(defn build-board
-  []
-  (vec (repeat width (vec (repeat height nil)))))
+(def board (vec (repeat width (vec (repeat height nil)))))
 
-(defn board
-  [board]
-  [:div#board
-   (for [y (range (count board))]
-     [:div.row
-       (for [x (range (count (first board)))]
-         [:div.cell "_"])])])
+(defn- populate
+  [board snake]
+  (reduce (fn [board coords]
+            (assoc-in board coords :snake))
+          board
+          snake))
+
+(defn- board-cell
+  [value]
+  (case value
+    nil [:div.cell "_"]
+    :snake [:div.cell "█"]))
+
+(defn game
+  [snake]
+  (let [populated-board (populate board @snake)]
+    [:div#board
+     (for [y (range (count populated-board))]
+       ^{:key (str "row" y)} [:div.row
+        (for [x (range (count (first populated-board)))]
+          (let [value (get-in populated-board [y x])]
+            ^{:key (str y x)} [board-cell value]))])]))
