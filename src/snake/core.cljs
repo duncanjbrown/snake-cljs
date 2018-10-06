f5d517633df321172fa2d70225429ba5f5bf68f7
Duncan Brown <duncanjbrown@gmail.com>
2018-10-06 23:30:04 +0100

    Add snake.core.tick
    This will be our main game loop. At the moment it combines a new "direction"
    property in the game state with a the current state of the snake, and updates
    the state with the next snake.
    
    As part of this change, never call (main) it from the CLJS - put it in the HTML.
    This is because we don't want a loop to start on each reload.

--- src/snake/core.cljs
+++ src/snake/core.cljs
@@ -1,9 +1,17 @@
-(ns ^:figwheel-always snake.core
+(ns snake.core
   (:require [reagent.core :as reagent]
             [snake.board :as board]
             [snake.snake :as snk]))
 
-(defonce state (reagent/atom {:snake (snk/new-snake)}))
+(defonce state (reagent/atom {:direction [0 1] :snake (snk/new-snake) :speed 100}))
 
-(when-let [element (js/document.getElementById "app")]
-  (reagent/render-component [board/game (reagent/cursor state [:snake])] element))
+(defn tick
+  []
+  (swap! state assoc :snake (snk/move-snake (:snake @state) (:direction @state) 10))
+  (js/setTimeout #(reagent/next-tick tick) (:speed @state)))
+
+(defn main []
+  (when-let [element (js/document.getElementById "app")]
+    (do
+      (reagent/render-component [board/game (reagent/cursor state [:snake])] element)
+      (tick))))
